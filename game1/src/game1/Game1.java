b78b02bb7129e10a651028f4cebfc0006b317cbd
GalenMarch <gamarch@vassar.edu>
2015-03-02 15:08:27 -0500

    Game Manual

---
+++ game1/src/game1/Game1.java
@@ -0,0 +1,38 @@
+package game1;
+
+public class Game1 {
+
+    
+    public static void main(String[] args) {
+        
+        // Moving Blocks Project game manual:
+        //
+        // DODGING FALLING BLOCKS
+        //
+        // A field of play where the blocks move:
+        // My game will be played on a black background
+        //
+        // A set of "live" blocks controlled by the player:
+        // The player will control a single block that can move left
+        // and right across the bottom of the playing field.
+        //
+        // A set of "dead" blocks that are no longer plater controlled:
+        // The object of my game is to dodge randomly-generated blocks as they
+        // fall from the top of the playing field.  In addition, the player 
+        // has three lives.  Each time the player is hit with a block, the player
+        // controlled block "dies" and remains in the spot where it was struck, reducing
+        // the amount of space the player has to dodge blocks later in the game.  This 
+        //will ideally add more challenge to the game as it progresses.
+        //
+        // A scoring system:
+        // For each second the player remains alive, ten points will be awarded
+        //
+        // A win or fail state:
+        // The game is lost when the player loses all three lives (i.e. when the player
+        // is hit with falling blocks three times)
+        //
+        // A control mechanism:
+        // The player will use the left and right arrow keys to control their block
+    }
+    
+}
